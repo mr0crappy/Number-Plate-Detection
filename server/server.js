95e6de0af6317c1d853a019335b8f9f7e16ef3da
mr0crappy <kpratyush82@gmail.com>
2026-07-16 03:38:47 +0530

    DEBUG DEPLOYMENT

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+console.log("CLIENT_URL =", process.env.CLIENT_URL);
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
